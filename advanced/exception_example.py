db97e51cdb051a7181989b8887abdfe37276546c
Rishi Bakliwal <134679229+TechTitanR@users.noreply.github.com>
2025-05-24 13:58:26 +0530

    Create exception_example.py

---
+++ advanced/exception_example.py
@@ -0,0 +1,12 @@
+# exception_example.py
+
+try:
+    num = int(input("Enter a number: "))
+    result = 10 / num
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Error: Cannot divide by zero!")
+except ValueError:
+    print("Error: Invalid input, please enter a number.")
+finally:
+    print("Program ended.")
